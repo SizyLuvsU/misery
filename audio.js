c6fdc7c15796d9aa7927013e56e0bf266ac1eb20
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-10-07 16:06:34 +0300

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,7 @@
+
+function play() {
+var audio = new Audio('./audio.mp3');
+audio.loop = true;
+audio.play();
+audio.volume = 0.20
+}
---
+++ typedpage.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : ['Very secret page :D ','Made by @poliisi.fi'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
