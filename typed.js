d44f482b842a6e5ab086125206a5317d643cf189
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-10-10 10:29:42 +0300

    Delete typed.js

--- typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : ['I stare at your pictures for hours on end  Dreaming, fantasizing about holding you. Feeling my hands on your bare skin','Just being with you But I know itll never happen, But I cant stop myself from fantasizing','I think about you for hours until I realize how much time Ive spent thinking of a scenario that will never happen','Youre so beautiful and funny And anytime I see your picture or hear your name. My stomach flips and I get butterflies. Every word you say sends goosebumps all over my body','Ive tried for so long not to look at you to not look at pictures of you to cut off all contact with you But I cant stop','I cant stop how I feel I dont know if Ill ever stop feeling this way. At least not for a long time','I see you with him it makes me sick to my stomach. And although that should put me off you, it doesnt It only makes me want you more','If you knew how I felt, I wouldnt even be writing this shitI wouldnt be so lonely. Youd understand how much you mean to me.','You mean so much to me','I wish youd look at me the way I look at you I wish I could be with you I just want to feel the warmth of your body next to mine. To see your smile I want to play with your hair. See it shine under the sun as it hits you just right','I want to watch the sun sink behind the heavens with you Talk, laugh, make out Anything I just want anything from you'],
-  typeSpeed : 40,
-  delaySpeed : 100,
-  showCursor: false,
-  loop : true
-})
