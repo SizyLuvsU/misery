a0348602cc580435a6ebdb8ae7f3d6fa954732fc
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-10-10 10:29:03 +0300

    Delete index1.js

--- index1.js
+++
@@ -1,8 +0,0 @@
-// Get the modal
-var modal = document.getElementById("enterModal");
-modal.style.display = "block";
-// Get the button that opens the modal
-
-function enter(){
-  modal.style.display = "none"
-}
